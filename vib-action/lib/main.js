2d706b756df080cc96b9a63cdb38ded0f41f987f
Gloria Delgado <gloriad@vmware.com>
2022-01-18 12:58:23 +0100

    Adding new tests

--- vib-action/lib/main.js
+++ vib-action/lib/main.js
@@ -36,24 +36,27 @@ const constants = __importStar(require("./constants"));
 const core = __importStar(require("@actions/core"));
 const path = __importStar(require("path"));
 const axios_1 = __importDefault(require("axios"));
+const axios_2 = __importDefault(require("axios"));
 const fs_1 = __importDefault(require("fs"));
 const util_1 = __importDefault(require("util"));
-const root = path.join(__dirname, '..');
+const root = process.env.GITHUB_WORKSPACE
+    ? path.join(process.env.GITHUB_WORKSPACE, ".")
+    : path.join(__dirname, "..");
 const cspClient = axios_1.default.create({
     baseURL: `${process.env.CSP_API_URL}`,
     timeout: 3000,
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
 });
 const vibClient = axios_1.default.create({
     baseURL: `${process.env.VIB_PUBLIC_URL}`,
     timeout: 3000,
-    headers: { 'Content-Type': 'application/json' }
+    headers: { "Content-Type": "application/json" },
 });
 let cachedCspToken = null;
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         //TODO: Refactor so we don't need to do this check
-        if (process.env['JEST_TESTS'] === 'true')
+        if (process.env["JEST_TESTS"] === "true")
             return; // skip running logic when importing class for npm test
         yield runAction();
     });
@@ -71,8 +74,8 @@ function runAction() {
             core.info(`Created pipeline with id ${executionGraphId}.`);
             // Now wait until pipeline ends or times out
             let executionGraph = yield getExecutionGraph(executionGraphId);
-            while (!Object.values(constants.EndStates).includes(executionGraph['status'])) {
-                core.info(`Fetched execution graph with id ${executionGraphId}. Status: ${executionGraph['status']}`);
+            while (!Object.values(constants.EndStates).includes(executionGraph["status"])) {
+                core.info(`Fetched execution graph with id ${executionGraphId}. Status: ${executionGraph["status"]}`);
                 if (Date.now() - startTime >
                     constants.DEFAULT_EXECUTION_GRAPH_GLOBAL_TIMEOUT) {
                     //TODO: Allow user to override the global timeout via action input params
@@ -84,14 +87,14 @@ function runAction() {
             }
             core.info(`Generating action outputs.`);
             //TODO: Improve existing tests to verify that outputs are set
-            core.setOutput('execution-graph', executionGraph);
+            core.setOutput("execution-graph", executionGraph);
             // TODO: Fetch logs and results
             // TODO: Upload logs and results as artifacts
-            if (!Object.values(constants.EndStates).includes(executionGraph['status'])) {
+            if (!Object.values(constants.EndStates).includes(executionGraph["status"])) {
                 core.setFailed(`Execution graph ${executionGraphId} has timed out.`);
             }
             else {
-                if (executionGraph['status'] === constants.EndStates.FAILED) {
+                if (executionGraph["status"] === constants.EndStates.FAILED) {
                     core.setFailed(`Execution graph ${executionGraphId} has failed.`);
                 }
                 else {
@@ -110,13 +113,25 @@ exports.runAction = runAction;
 function getExecutionGraph(executionGraphId) {
     return __awaiter(this, void 0, void 0, function* () {
         core.debug(`Getting execution graph with id ${executionGraphId}`);
-        if (typeof process.env.VIB_PUBLIC_URL === 'undefined') {
-            throw new Error('VIB_PUBLIC_URL environment variable not found.');
+        if (typeof process.env.VIB_PUBLIC_URL === "undefined") {
+            throw new Error("VIB_PUBLIC_URL environment variable not found.");
         }
         const apiToken = yield getToken({ timeout: constants.CSP_TIMEOUT });
-        const response = yield vibClient.get(`/v1/execution-graphs/${executionGraphId}`, { headers: { Authorization: `Bearer ${apiToken}` } });
-        //TODO: Handle response codes
-        return response.data;
+        try {
+            const response = yield vibClient.get(`/v1/execution-graphs/${executionGraphId}`, { headers: { Authorization: `Bearer ${apiToken}` } });
+            //TODO: Handle response codes
+            return response.data;
+        }
+        catch (err) {
+            if (axios_2.default.isAxiosError(err) && err.response) {
+                if (err.response.status == 404) {
+                    core.debug(err.response.data.detail);
+                    throw new Error(err.response.data.detail);
+                }
+                throw new Error(err.response.data.detail);
+            }
+            throw err;
+        }
     });
 }
 exports.getExecutionGraph = getExecutionGraph;
@@ -124,28 +139,28 @@ function createPipeline(config) {
     var _a;
     return __awaiter(this, void 0, void 0, function* () {
         core.debug(`Config: ${config}`);
-        if (typeof process.env.VIB_PUBLIC_URL === 'undefined') {
-            throw new Error('VIB_PUBLIC_URL environment variable not found.');
+        if (typeof process.env.VIB_PUBLIC_URL === "undefined") {
+            throw new Error("VIB_PUBLIC_URL environment variable not found.");
         }
         const apiToken = yield getToken({ timeout: constants.CSP_TIMEOUT });
         try {
-            const folderName = path.join(root, constants.DEFAULT_BASE_FOLDER);
-            const filename = path.join(folderName, constants.DEFAULT_PIPELINE);
+            const folderName = path.join(root, config.baseFolder);
+            const filename = path.join(folderName, config.pipeline);
             core.debug(`Reading pipeline file from ${filename}`);
             const pipeline = fs_1.default.readFileSync(filename).toString();
             core.debug(`Sending pipeline: ${util_1.default.inspect(pipeline)}`);
             //TODO: Define and replace different placeholders: e.g. for values, content folders (goss, jmeter), etc.
-            const response = yield vibClient.post('/v1/pipelines', pipeline, {
-                headers: { Authorization: `Bearer ${apiToken}` }
+            const response = yield vibClient.post("/v1/pipelines", pipeline, {
+                headers: { Authorization: `Bearer ${apiToken}` },
             });
             core.debug(`Got create pipeline response data : ${JSON.stringify(response.data)}, headers: ${util_1.default.inspect(response.headers)}`);
             //TODO: Handle response codes
-            const locationHeader = (_a = response.headers['location']) === null || _a === void 0 ? void 0 : _a.toString();
-            if (typeof locationHeader === 'undefined') {
-                throw new Error('Location header not found');
+            const locationHeader = (_a = response.headers["location"]) === null || _a === void 0 ? void 0 : _a.toString();
+            if (typeof locationHeader === "undefined") {
+                throw new Error("Location header not found");
             }
             core.debug(`Location Header: ${locationHeader}`);
-            const executionGraphId = locationHeader.substring(locationHeader.lastIndexOf('/') + 1);
+            const executionGraphId = locationHeader.substring(locationHeader.lastIndexOf("/") + 1);
             return executionGraphId;
         }
         catch (error) {
@@ -160,25 +175,25 @@ function getToken(input) {
         //const config = loadConfig()
         core.debug(`Checking CSP API token... Cached token: ${cachedCspToken}`);
         core.debug(typeof process.env.CSP_API_TOKEN);
-        if (typeof process.env.CSP_API_TOKEN === 'undefined') {
-            throw new Error('CSP_API_TOKEN secret not found.');
+        if (typeof process.env.CSP_API_TOKEN === "undefined") {
+            throw new Error("CSP_API_TOKEN secret not found.");
         }
-        if (typeof process.env.CSP_API_URL === 'undefined') {
-            throw new Error('CSP_API_URL environment variable not found.');
+        if (typeof process.env.CSP_API_URL === "undefined") {
+            throw new Error("CSP_API_URL environment variable not found.");
         }
         if (cachedCspToken != null && cachedCspToken.timestamp > Date.now()) {
             return cachedCspToken.access_token;
         }
         try {
-            const response = yield cspClient.post('/csp/gateway/am/api/auth/api-tokens/authorize', `grant_type=refresh_token&api_token=${process.env.CSP_API_TOKEN}`);
+            const response = yield cspClient.post("/csp/gateway/am/api/auth/api-tokens/authorize", `grant_type=refresh_token&api_token=${process.env.CSP_API_TOKEN}`);
             //TODO: Handle response codes
-            if (typeof response.data === 'undefined' ||
-                typeof response.data.access_token === 'undefined') {
-                throw new Error('Could not fetch access token.');
+            if (typeof response.data === "undefined" ||
+                typeof response.data.access_token === "undefined") {
+                throw new Error("Could not fetch access token.");
             }
             cachedCspToken = {
                 access_token: response.data.access_token,
-                timestamp: Date.now() + input.timeout
+                timestamp: Date.now() + input.timeout,
             };
             return response.data.access_token;
         }
@@ -190,26 +205,32 @@ function getToken(input) {
 exports.getToken = getToken;
 function loadConfig() {
     return __awaiter(this, void 0, void 0, function* () {
-        const pipeline = constants.DEFAULT_PIPELINE;
-        const baseFolder = constants.DEFAULT_BASE_FOLDER;
-        const folderName = path.join(root, constants.DEFAULT_BASE_FOLDER);
+        let pipeline = core.getInput("pipeline");
+        let baseFolder = core.getInput("config");
+        if (pipeline === "") {
+            pipeline = constants.DEFAULT_PIPELINE;
+        }
+        if (baseFolder === "") {
+            baseFolder = constants.DEFAULT_BASE_FOLDER;
+        }
+        const folderName = path.join(root, baseFolder);
         if (!fs_1.default.existsSync(folderName)) {
             throw new Error(`Could not find base folder at ${folderName}`);
         }
-        const filename = path.join(folderName, constants.DEFAULT_PIPELINE);
+        const filename = path.join(folderName, pipeline);
         if (!fs_1.default.existsSync(filename)) {
             throw new Error(`Could not find pipeline at ${baseFolder}/${pipeline}`);
         }
         return {
             pipeline,
-            baseFolder
+            baseFolder,
         };
     });
 }
 exports.loadConfig = loadConfig;
 /*eslint-disable @typescript-eslint/explicit-function-return-type, @typescript-eslint/promise-function-async*/
 //TODO: Enable linter
-const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 /*eslint-enable */
 function reset() {
     return __awaiter(this, void 0, void 0, function* () {
--- vib-action/src/constants.ts
+++ vib-action/src/constants.ts
@@ -3,40 +3,40 @@
  *
  * @default '.cp'
  */
-export const DEFAULT_BASE_FOLDER = '.cp'
+export const DEFAULT_BASE_FOLDER = ".cp";
 
 /**
  * Base VIB pipeline file
  *
  * @default 'cp-pipeline.json'
  */
-export const DEFAULT_PIPELINE = 'cp-pipeline.json'
+export const DEFAULT_PIPELINE = "cp-pipeline.json";
 
 /**
  * Max waiting time for an execution graph to complete
  *
  * @default 90 minutes
  */
-export const DEFAULT_EXECUTION_GRAPH_GLOBAL_TIMEOUT = 90 * 60 * 1000
+export const DEFAULT_EXECUTION_GRAPH_GLOBAL_TIMEOUT = 90 * 60 * 1000;
 
 /**
  * Interval for checking the execution graph status
  *
  * @default 30 seconds
  */
-export const DEFAULT_EXECUTION_GRAPH_CHECK_INTERVAL = 30 * 1000 // 30 seconds
+export const DEFAULT_EXECUTION_GRAPH_CHECK_INTERVAL = 30 * 1000; // 30 seconds
 
 /**
  * Max caching time for valid CSP tokens
  *
  * @default 10 minutes
  */
-export const CSP_TIMEOUT: number = 10 * 60 * 1000 // 10 minutes
+export const CSP_TIMEOUT: number = 10 * 60 * 1000; // 10 minutes
 
 /**
  * Valid states indicating that the execution graph processing has completed
  */
 export enum EndStates {
-  SUCCEEDED = 'SUCCEEDED',
-  FAILED = 'FAILED'
+  SUCCEEDED = "SUCCEEDED",
+  FAILED = "FAILED",
 }
