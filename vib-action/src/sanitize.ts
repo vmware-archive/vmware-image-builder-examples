39aebbab6561f483d9800e66ce8f35725a442787
Martin Perez <martinpe@vmware.com>
2022-01-21 10:01:55 +0100

    Added a sanitize tool for generating report filenames

---
+++ vib-action/src/sanitize.ts
@@ -0,0 +1,18 @@
+const illegalRe = /[\/\?<>\\:\*\|"]/g;
+const controlRe = /[\x00-\x1f\x80-\x9f]/g;
+const reservedRe = /^\.+$/;
+
+export function sanitize(
+    input: string,
+    replacement: string
+  ): string {
+
+  if (typeof input !== 'string') {
+    throw new Error('Input must be string');
+  }
+
+  return input
+    .replace(illegalRe, replacement)
+    .replace(controlRe, replacement)
+    .replace(reservedRe, replacement);
+}
