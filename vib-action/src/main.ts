307c449a66e7f41714e38b91c4bff3dc299ea778
Gloria Delgado <gloriad@vmware.com>
2022-02-01 13:54:30 +0100

    Replacing errors with warnings when downloading logs and reports

--- vib-action/src/main.ts
+++ vib-action/src/main.ts
@@ -514,7 +514,7 @@ export async function getRawReports(
   } catch (err) {
     if (axios.isAxiosError(err) && err.response) {
       // Don't throw error if we cannot fetch a report
-      core.error(`Received error while fetching reports for task ${taskId}. Error code: ${err.response.status}. Message: ${err.response.statusText}`)
+      core.warning(`Received error while fetching reports for task ${taskId}. Error code: ${err.response.status}. Message: ${err.response.statusText}`)
       return []
     } else {
       throw err
@@ -547,7 +547,7 @@ export async function getRawLogs(
   } catch (err) {
     if (axios.isAxiosError(err) && err.response) {
       // Don't throw error if we cannot fetch a log
-      core.error(`Received error while fetching logs for task ${taskId}. Error code: ${err.response.status}. Message: ${err.response.statusText}`)
+      core.warning(`Received error while fetching logs for task ${taskId}. Error code: ${err.response.status}. Message: ${err.response.statusText}`)
       return null
     } else {
       throw err
