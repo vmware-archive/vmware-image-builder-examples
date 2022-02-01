427e2ce344592fa2fba1bc095407d5cde607f984
Martin Perez <martinpe@vmware.com>
2022-02-01 18:27:12 +0100

    Add support for reading GITHUB_SHA from the github event information. Solves #11

--- vib-action/src/constants.ts
+++ vib-action/src/constants.ts
@@ -3,35 +3,35 @@
  *
  * @default '.vib'
  */
-export const DEFAULT_BASE_FOLDER = ".vib"
+export const DEFAULT_BASE_FOLDER = ".vib";
 
 /**
  * Base VIB pipeline file
  *
  * @default 'vib-pipeline.json'
  */
-export const DEFAULT_PIPELINE = "vib-pipeline.json"
+export const DEFAULT_PIPELINE = "vib-pipeline.json";
 
 /**
  * Max waiting time for an execution graph to complete
  *
  * @default 90 minutes
  */
-export const DEFAULT_EXECUTION_GRAPH_GLOBAL_TIMEOUT = 90 * 60 * 1000
+export const DEFAULT_EXECUTION_GRAPH_GLOBAL_TIMEOUT = 90 * 60 * 1000;
 
 /**
  * Interval for checking the execution graph status
  *
  * @default 30 seconds
  */
-export const DEFAULT_EXECUTION_GRAPH_CHECK_INTERVAL = 30 * 1000 // 30 seconds
+export const DEFAULT_EXECUTION_GRAPH_CHECK_INTERVAL = 30 * 1000; // 30 seconds
 
 /**
  * Max caching time for valid CSP tokens
  *
  * @default 10 minutes
  */
-export const CSP_TIMEOUT: number = 10 * 60 * 1000 // 10 minutes
+export const CSP_TIMEOUT: number = 10 * 60 * 1000; // 10 minutes
 
 /**
  * Valid states indicating that the execution graph processing has completed
@@ -46,14 +46,14 @@ export enum EndStates {
  *
  * @default GKE: 91d398a2-25c4-4cda-8732-75a3cfc179a1
  */
- export const DEFAULT_TARGET_PLATFORM = '91d398a2-25c4-4cda-8732-75a3cfc179a1' // GKE
+export const DEFAULT_TARGET_PLATFORM = "91d398a2-25c4-4cda-8732-75a3cfc179a1"; // GKE
 
- /**
-  * Default VIB public URL. This endpoint requires authentication
-  */
- export const DEFAULT_VIB_PUBLIC_URL = "https://cp.bromelia.vmware.com"
+/**
+ * Default VIB public URL. This endpoint requires authentication
+ */
+export const DEFAULT_VIB_PUBLIC_URL = "https://cp.bromelia.vmware.com";
 
 /**
  * Default URL to the VMware Cloud Services Platform. This service provides identity access
  */
- export const DEFAULT_CSP_API_URL = "https://console.cloud.vmware.com"
+export const DEFAULT_CSP_API_URL = "https://console.cloud.vmware.com";
